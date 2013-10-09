abf2e6e95d8db1e098bc79041c9e1b0fef868618
Hoylen Sue <hoylen@hoylen.com>
2013-10-09 18:38:55 +1000

    Added licence header.

--- q-image-create.sh
+++ q-image-create.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 #
-# Note: using "bash" instead of "sh" so that the source
-# command is available and handles the "read -s" in the RC file.
+# Utility to help in the creation of virtual machine images.
+#
+# Copyright (C) 2013, Queensland Cyber Infrastructure Foundation Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see {http://www.gnu.org/licenses/}.
+#----------------------------------------------------------------
+
+# Note: script explicitly uses "bash" instead of "sh" so that the
+# "source" command is available and handles the "read -s" in the RC
+# file.
 
 PROG=`basename $0`
 
